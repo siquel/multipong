785f46c3e65d31c2442c6b58e4ec21e9469d63ed
siquel <squual@kahvipaussi.net>
2016-09-18 01:02:34 +0300

    Use address manipulation functions from jn

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -28,17 +28,15 @@ int main(int, char**)
     initialize();
     jkn::IPAddress addr = {};
     addr.m_port = 1337;
-    addr.m_type = jkn::IPAddressType::IPv4;
-    addr.m_ipv4 = uint32_t(127) | (uint32_t(0) << 8) | (uint32_t(0) << 16) | (uint32_t(1) << 24);
+    
+    jkn::addressSetHost(addr, 127, 0, 0, 1);
+
+    char ip[128] = {};
+    jkn::addressGetHostIp(addr, ip, sizeof(ip));
 
     jkn::UDPSocket socket(addr);
 
-    printf("Starting server on ip %d.%d.%d.%d : %d\n",
-        (addr.m_ipv4 & 0xff),
-        (addr.m_ipv4 >> 8) & 0xff,
-        (addr.m_ipv4 >> 16) & 0xff,
-        (addr.m_ipv4 >> 24) & 0xff,
-        addr.m_port);
+    printf("Starting server on ip %s:%d\n", ip, addr.m_port);
 
     deinitialize();
     return 0;
